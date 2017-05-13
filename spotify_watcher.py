b78f78849bc2a5505789c1dc00836460770e3c99
Erik Bjäreholt <erik@bjareho.lt>
2017-05-13 15:04:58 +0200

    fixed token renewal

--- spotify_watcher.py
+++ spotify_watcher.py
@@ -81,21 +81,22 @@ def main():
 
     sp = auth(username)
     while True:
-        track = get_current_track(sp)
-        # from pprint import pprint
-        # pprint(track)
+        try:
+            track = get_current_track(sp)
+            # from pprint import pprint
+            # pprint(track)
+        except spotipy.client.SpotifyException as e:
+            print("\nToken expired, trying to refresh\n")
+            sp = auth(username)
+            continue
+
         print_statusline("Waiting for track to start playing...")
         if track:
             track_data = data_from_track(track)
             song_td = timedelta(seconds=track['progress_ms'] / 1000)
             song_time = int(song_td.seconds / 60), int(song_td.seconds % 60)
             print_statusline("Current track ({}:{}): {title} - {artist} ({album})".format(*song_time, **track_data))
-            try:
-                aw.heartbeat(bucketname, Event(timestamp=datetime.now(timezone.utc), data=track_data), pulsetime=poll_interval + 1)
-                pass
-            except spotipy.client.SpotifyException as e:
-                print("\nToken expired, trying to refresh\n")
-                sp = auth(username)
+            aw.heartbeat(bucketname, Event(timestamp=datetime.now(timezone.utc), data=track_data), pulsetime=poll_interval + 1)
 
         sleep(poll_interval)
     else:
